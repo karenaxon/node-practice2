838323bbc4754082249942e115a371a41fbc948d
Kaxon <karenaxon@users.noreply.github.com>
2022-10-27 14:09:56 -0700

    Add write new file

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const path = require('path');
 
 fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
   if(err) throw err;
-  console.log(data)
+  // console.log(data)
 })
 
 // exit on uncaught errors
 process.on('uncaughtException', err => {
   console.log(`There was an uncaught error: ${err}`);
   process.exit(1);
+})
+
+fs.writeFile(path.join(__dirname, 'files', 'file1.txt'),`If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.`, (err) => {
+  if(err) throw err;
+  console.log('\nThe file has been written!')
 })
