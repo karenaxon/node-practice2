5ebc067f165abb4afbbac105a64e7d7adaca2247
Kaxon <karenaxon@users.noreply.github.com>
2022-10-30 12:33:52 -0700

    Add events log emitter

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-const fsPromises = require('fs').promises;
-const path = require('path');
+const logEvents = require('./logEvents');
 
-const fileOps = async() => {
-  try{
-    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8');
-    console.log(data);
-    //Delete original file
-    fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'));
-    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseFile.txt'), data);
-    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseFile.txt'), `\n\n-* NEW *-\nIf you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.`);
-    await fsPromises.rename(path.join(__dirname, 'files', 'promiseFile.txt'),path.join(__dirname, 'files', 'file1.txt'));
-    
-  } catch (err) {
-    console.log(err)
-  }
-}
+const EventEmitter = require('events');
 
-fileOps();
+class MyEmitter extends EventEmitter{};
 
+//init object
+const myEmitter = new MyEmitter();
 
+//add listener for log event
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+  //Emit event
+  myEmitter.emit('log', 'Log event emitted!');
+}, 2000);
